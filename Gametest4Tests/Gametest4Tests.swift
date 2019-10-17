a9de5b8a94d3d2f9476298330c9c36ae478e2317
Preetwinder kaur <Pbrar767@gmail.com>
2019-10-17 12:08:03 -0400

    commit1

---
+++ Gametest4Tests/Gametest4Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Gametest4Tests.swift
+//  Gametest4Tests
+//
+//  Created by MacStudent on 2019-10-17.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import XCTest
+@testable import Gametest4
+
+class Gametest4Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
